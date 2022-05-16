69ea31c5adc537e3a083c61ae12e3182b2e77a7f
Arseniy Zhiltsov <arseniy.zhiltsov@ccsteam.ru>
2022-05-16 15:16:03 +0300

    Use pydantic aliases in response and request

--- pybotx_smartapp_rpc/models/request.py
+++ pybotx_smartapp_rpc/models/request.py
@@ -3,6 +3,11 @@ from typing import Any, Dict, Literal
 from pydantic import BaseModel
 
 
+class RPCArgsBaseModel(BaseModel):
+    class Config:
+        allow_population_by_field_name = True
+
+
 class RPCRequest(BaseModel):
     method: str
     type: Literal["smartapp_rpc"]
