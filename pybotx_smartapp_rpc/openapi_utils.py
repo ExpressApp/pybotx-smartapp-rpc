4261a8a4d9d451e90794730131e1e517dca70555
Kirill Lisov <kirill.lisov@ccsteam.ru>
2023-11-08 17:11:20 +0300

    feat: move model defenition function from FastApi

--- pybotx_smartapp_rpc/openapi_utils.py
+++ pybotx_smartapp_rpc/openapi_utils.py
@@ -51,6 +51,23 @@ def get_rpc_flat_models_from_routes(
     )
 
 
+def get_rpc_model_definitions(
+    *,
+    flat_models: Set[Union[Type[BaseModel], Type[Enum]]],
+    model_name_map: Dict[Union[Type[BaseModel], Type[Enum]], str],
+) -> Dict[str, Any]:
+    definitions: Dict[str, Dict[str, Any]] = {}
+    for model in flat_models:
+        m_schema, m_definitions, m_nested_models = model_process_schema(
+            model, model_name_map=model_name_map, ref_prefix=REF_PREFIX
+        )
+        definitions.update(m_definitions)
+        model_name = model_name_map[model]
+        if "description" in m_schema:
+            m_schema["description"] = m_schema["description"].split("\f")[0]
+        definitions[model_name] = m_schema
+    return definitions
+
 def get_openapi_operation_rpc_args(
     *,
     body_field: Optional[ModelField],
