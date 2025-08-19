e1959f3a04ce62c344f3c1c95e6fa11b7dfb93e9
gubarik.vladimir <vladimir.gubarik@ccsteam.ru>
2025-08-19 18:14:53 +0300

    add fastapi openapi

---
+++ pybotx_smartapp_rpc/fastapi_utils/security.py
@@ -0,0 +1,68 @@
+import re
+from typing import Any
+from uuid import UUID, uuid4
+
+from fastapi import HTTPException
+from fastapi.encoders import jsonable_encoder
+from fastapi.security import APIKeyHeader
+from fastapi.security.base import SecurityBase
+from pydantic import ValidationError, BaseModel
+from starlette.status import HTTP_403_FORBIDDEN
+
+
+def get_openapi_security_definitions(
+    security_component: SecurityBase,
+) -> tuple[dict[str, Any], dict[str, Any]]:
+    """Get the security definitions and operation security for a security component."""
+    security_definition = jsonable_encoder(
+        security_component.model,
+        by_alias=True,
+        exclude_none=True,
+    )
+    security_name = security_component.scheme_name
+    security_definitions = {security_name: security_definition}
+    operation_security = {security_name: []}  # type: ignore
+    return security_definitions, operation_security
+
+DOCS = """Установка параметров для выполнение RPC методов.
+
+* `bot_id` - huid бота. Необязательное поле.
+* `sender_huid` - huid пользователя. Необязательное поле.
+* `sender_udid` - udid пользователя. Необязательное поле.
+* `chat_id` - id чата. Необязательное поле.
+
+**Example**: `bot_id=UUID&sender_huid=UUID&sender_udid=UUID&chat_id=UUID`"""
+
+class RPCAuthConfig(BaseModel):
+    bot_id: UUID
+    sender_huid: UUID = uuid4()
+    sender_udid: UUID = uuid4()
+    chat_id: UUID = uuid4()
+
+class RPCAuth(APIKeyHeader):
+    PATTERN = "([^?=&]+)=([^&]*)"
+
+    def __init__(self,*,bot_id:UUID, scheme_name:str="RPC Auth", name:str="X-RPC-AUTH", description:str=DOCS, **kwargs) -> None:
+        self.bot_id = bot_id
+        super().__init__(scheme_name=scheme_name,name=name, description=description, **kwargs)
+
+    async def __call__(self, request: Request) -> RPCAuthConfig:  # type: ignore
+        api_key = request.headers.get(self.model.name)
+        if not api_key:
+            return RPCAuthConfig(bot_id=self.bot_id)
+
+        params = re.findall(self.PATTERN, api_key)  # noqa: WPS110
+        if not params:
+            raise HTTPException(
+                status_code=HTTP_403_FORBIDDEN, detail="Invalid RPC Auth format"
+            )
+        try:
+            params_dict = dict(params)
+            if (bot_id:=params_dict.get("bot_id")) is None:
+                params_dict["bot_id"] = bot_id
+
+            config = RPCAuthConfig(**params_dict)
+        except ValidationError as ex:
+            raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail=str(ex))
+
+        return config
